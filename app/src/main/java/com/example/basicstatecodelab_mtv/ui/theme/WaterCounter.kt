81786f7f2f7b4f54597f7b55f15ee44d09de6c5a
monts31 <s23120178@alumnos.itsur.edu.mx>
2026-09-23 17:39:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/basicstatecodelab_mtv/ui/theme/WaterCounter.kt
@@ -0,0 +1,22 @@
+package com.example.basicstatecodelab_mtv.ui.theme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text("You've had ${count.value} glasses.",)
+        Button(onClick = {count.value++}, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/basicstatecodelab_mtv/ui/theme/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.example.basicstatecodelab_mtv.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
