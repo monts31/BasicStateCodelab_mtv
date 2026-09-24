54cd07b54eb37b6dee0786b2b4034d18f1e93e93
monts31 <s23120178@alumnos.itsur.edu.mx>
2026-09-23 19:49:12 -0600

    Finalizacion de la app

--- app/src/main/java/com/example/basicstatecodelab_mtv/ui/theme/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab_mtv/ui/theme/WellnessScreen.kt
@@ -1,9 +1,30 @@
 package com.example.basicstatecodelab_mtv.ui.theme
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
 }
+
